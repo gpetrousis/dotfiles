f09be31d1a5d97c3307721e6809825758a8af596
Georgios Petrousis <gpetrousis@gmail.com>
2022-05-28 19:39:24 +0200

    Remove old Makefiles and README.md files

--- scripts/common.sh
+++ scripts/common.sh
@@ -75,3 +75,7 @@ if [[ $all -eq 1 ]]; then
     fi
     configs=( $AVAILABLE_CONFIGS )
 fi
+$ git branch -m OLD-BRANCH-NAME NEW-BRANCH-NAME
+$ git fetch origin
+$ git branch -u origin/NEW-BRANCH-NAME NEW-BRANCH-NAME
+$ git remote set-head origin -a
